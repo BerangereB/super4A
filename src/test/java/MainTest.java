17e3771d477da6ed7ce563b8158399131201ae75
int0TheW1ld <courreau@et.esiea.fr>
2019-01-15 11:57:43 +0100

    First class test (MainTest) to test Codecov conf

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Quentin on 15/01/2019
+ * @project super4A - PACKAGE_NAME
+ */
+class MainTest {
+	@Test
+	void main() {
+		assert(true==true);
+	}
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,7 @@
+/**
+ * @author Quentin on 15/01/2019
+ * @project super4A - PACKAGE_NAME
+ */
+public class Test {
+
+}
