c8d9b33ab13b88ef60c8980855475cf1ff85a8eb
int0TheW1ld <courreau@et.esiea.fr>
2019-01-15 11:39:16 +0100

    Init Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Quentin on 15/01/2019
+ * @project super4A - PACKAGE_NAME
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
