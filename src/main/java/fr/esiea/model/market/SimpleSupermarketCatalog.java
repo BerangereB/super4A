3b72754890f6c2183302854814e115270e980ab7
Berangere <bourdoulous@et.esiea.fr>
2019-02-26 10:42:06 +0100

    Change of HashMap into LinkedHashMap to keep the order of adding products when converting into JSON format

--- src/main/java/fr/esiea/model/market/SimpleSupermarketCatalog.java
+++ src/main/java/fr/esiea/model/market/SimpleSupermarketCatalog.java
@@ -1,11 +1,23 @@
 package fr.esiea.model.market;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class SimpleSupermarketCatalog implements SupermarketCatalog {
 
-	private Map<String, Product> products = new HashMap<String, Product>();
+	private Map<String, Product> products = new LinkedHashMap<String, Product>();
+
+	public SimpleSupermarketCatalog(){
+		Product toothbrush = new Product("toothbrush", ProductUnit.Each,0.99);
+		Product toothpaste = new Product("toothpaste", ProductUnit.Each,0.89);
+		Product apples = new Product("apples", ProductUnit.Kilo,1.99);
+		Product bananas = new Product("bananas", ProductUnit.Kilo,2.99);
+		addProduct(toothbrush);
+		addProduct(toothpaste);
+		addProduct(apples);
+	    addProduct(bananas);
+	}
 
 	@Override
 	public void addProduct(Product product) {
